e3256d930914467ab85f9d69b9f92f6445003d3b
peeemax <peeemax@hotmail.com>
2022-08-03 19:56:47 -0300

    Testado o Create no Banco de Dados MySQL

--- formulário.py
+++ formulário.py
@@ -3,11 +3,11 @@
 class Lead:
 
     def __init__(self, nome_completo, celular, email, data_nascimento, descricao):
-        self.descricao = descricao
         self.data_nascimento = data_nascimento
         self.email = email
         self.celular = celular
         self.nome_completo = nome_completo
+        self.descricao = descricao
 
 
 def registro_bd():
@@ -17,19 +17,24 @@ def registro_bd():
         host='localhost',
         user='Maximiano',
         password='Pemax1992',
-        database='bdcadastrofamilia',
+        database='formulario-lead',
     )
 
     cursor = conexao.cursor()
 
     # Comando para inserir e salvar dados inputados no banco de dados
-    comando = f'INSERT INTO formulario-lead (nome_completo, celular, email, data_nascimento, descricao) VALUES '
+    comando = f'INSERT INTO form_lead (nome_completo, celular, email, data_nascimento, descricao) VALUES ' \
+              f'("{lead.nome_completo}", "{lead.celular}", "{lead.email}", "{lead.data_nascimento}", "{lead.descricao}")'
     cursor.execute(comando)
     conexao.commit()
 
 
 if __name__ == "__main__":
-    lead1 = Lead('Pedro Maximiano', '19991002700', 'peeemax@hotmail.com', '13/09/1992', 'Saber mais sobre o serviços')
+    lead = Lead('Vitor', '192391293129', 'asdasd@dfgkasdo','2190931290', 'Saber')
+    lead1 = Lead('Pedro Maximiano', '19991002700', 'peeemax@hotmail.com', '13/09/1992', 'Saber de mais')
+
+    registro_bd()
+
     print(lead1.nome_completo, lead1.celular, lead1.email, lead1.data_nascimento, lead1.descricao)
 
 
