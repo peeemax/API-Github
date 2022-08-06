2ac609d1f54e057271a01162fed03b2a3cec5417
peeemax <peeemax@hotmail.com>
2022-08-05 21:39:08 -0300

    Adicionado os comentários para cada passo da API

--- api_github.py
+++ api_github.py
@@ -1,17 +1,19 @@
 import json
-
 import requests
 
+# Entrada de dado do usuário que deseja obter os dados do Github
 usuario_github = input('Digite o nome de seu usuário do github para registrar seus dados. ')
 
 link_github = f'https://api.github.com/users/{usuario_github}'
 
+# Conexão para acessar os dados da API do Github
 request = requests.get(link_github)
 
+# Dados transformados em Json para visualização
 dados_github = json.loads(request.content)
 
 
-def registro_bd():
+def registro_bd():  # Função para registrar os dados do usuário no Banco de dados
     # Conexão com banco de dados
     import mysql.connector
     conexao = mysql.connector.connect(
@@ -29,6 +31,5 @@ def registro_bd():
     cursor.execute(comando)
     conexao.commit()
 
+
 registro_bd()
-
-
